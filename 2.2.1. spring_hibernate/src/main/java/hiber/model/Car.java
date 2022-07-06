d3212eebd77e1bd87716796d34e64dee1f5ada40
tur <bonus90@mail.ru>
2022-07-06 17:40:53 +0300

    init

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -0,0 +1,67 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "series")
+    private int series;
+
+    @OneToOne(mappedBy = "userCar", cascade = CascadeType.ALL)
+    private User user;
+
+    public Car() {
+    }
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return model + " " + series;
+    }
+}
